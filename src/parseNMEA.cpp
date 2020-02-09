de4da31f960f78ba3e56af56c1762c37822db960
Tom <N0779393@ntu.ac.uk>
2020-02-09 20:43:58 +0000

    positionFromSetenceData working

--- src/parseNMEA.cpp
+++ src/parseNMEA.cpp
@@ -8,6 +8,11 @@
 
 namespace NMEA {
 
+// constants to be used for checking sentence data within functions
+std::list<std::string> const validLongitudes = {"E", "W"};
+std::list<std::string> const validLatitudes = {"N", "S"}; 
+std::list<std::string> static validStatuses = {"A", "V"};
+
 bool isWellFormedSentence(std::string s)
 {
     std::string sentence = s; 
@@ -78,10 +83,7 @@ SentenceData extractSentenceData(std::string s)
     std::string code = s.substr(3,3); // substring for NMEA code
     std::string fieldStr = s.substr(6); // substring after NMEA code
 
-    //unsigned int fieldLength = fields.length();
-
-
-    std::vector<std::string> data; // insert string data into a vector, excluding commas
+    std::vector<std::string> values; // insert string data into a vector, excluding commas
     int start = 1;
     int counter = 1; // start and counter variables declared for extraction loop
 
@@ -90,25 +92,87 @@ SentenceData extractSentenceData(std::string s)
 	// Push values to vector from substring if they exist before a "*" or ","
         if((fieldStr[counter] == '*') || (fieldStr[counter] == ',')){
             std::string pushdata = fieldStr.substr(start, counter - start);
-            data.push_back(pushdata);
+            values.push_back(pushdata);
             start = counter + 1;
         }
         counter++; // increment by 1 until extraction is finished
 
     }
-    return {code, data}; // Return NMEA's string code along with vector data
+    return {code, values}; // Return NMEA's string code along with vector data
 }
 
-GPS::Position positionFromSentenceData(SentenceData)
+GPS::Position positionFromSentenceData(SentenceData v)
 {
-    // Stub definition, needs implementing
-    return GPS::Earth::NorthPole;
+	if (v.second.empty()) // check for empty field before proceeding
+		throw std::invalid_argument("Empty field");
+
+	if (v.first == "GLL") {
+		if (v.second.size() != 5)
+			throw std::invalid_argument("GLL invalid, incorrect size"); 
+
+		std::stof(v.second[2]); // index 2 is longitude
+		std::stof(v.second[0]); // index 0 is latitude - convert both to float from string
+
+		if (!(std::find(validLongitudes.begin(), validLongitudes.end(), v.second[3]) != validLongitudes.end()))
+        	throw std::invalid_argument("Directional symbol is not valid");
+        char longitudeDirection = v.second[3][0];
+		if (!(std::find(validLatitudes.begin(), validLatitudes.end(), v.second[1]) != validLatitudes.end()))
+            throw std::invalid_argument("Directional symbol is not valid");
+        char latitudeDirection = v.second[1][0];
+
+		// format in correct NMEA data order - latitude value and direction, then latitude value and direction
+        GPS::Position values(v.second[0], latitudeDirection, v.second[2], longitudeDirection);
+        return values;
+    }
+
+	if (v.first == "RMC") {
+		if (v.second.size() != 11)
+			throw std::invalid_argument("RMC invalid, incorrect size");
+		std::stof(v.second[0]); // value for timestamp
+		std::stof(v.second[8]); // value for date
+		std::stof(v.second[4]); // value for longitude
+		std::stof(v.second[2]); // value for latitude
+
+		if (!(std::find(validLongitudes.begin(), validLongitudes.end(), v.second[5]) != validLongitudes.end()))
+            throw std::invalid_argument("Directional symbol is not valid");
+        char longitudeDirection = v.second[5][0]; // direction for longitude value
+		if (!(std::find(validLatitudes.begin(), validLatitudes.end(), v.second[3]) != validLatitudes.end()))
+            throw std::invalid_argument("Directional symbol is not valid");
+        char latitudeDirection = v.second[3][0]; // direction for latitude value
+
+		// format in correct NMEA data order again
+        GPS::Position values(v.second[2], latitudeDirection, v.second[4], longitudeDirection);
+        return values;
+    }
+
+	if (v.first == "GGA") {
+		if (v.second.size() != 14)
+			throw std::invalid_argument("GGA invalid, incorrect size");
+		std::stof(v.second[0]); //value for timestamp
+        std::stof(v.second[3]); //value for longitude
+        std::stof(v.second[1]); //value for latitude 
+
+		if (!(std::find(validLongitudes.begin(), validLongitudes.end(), v.second[4]) != validLongitudes.end()))
+            throw std::invalid_argument("Directional symbol is not valid");
+		char longitudeDirection = v.second[4][0]; //direction for longitude value
+		if (!(std::find(validLatitudes.begin(), validLatitudes.end(), v.second[2]) != validLatitudes.end()))
+            throw std::invalid_argument("Directional symbol is not valid");
+		char latitudeDirection = v.second[2][0]; //direction for latitude value
+
+		//format in correct NMEA sentence data order
+		GPS::Position values(v.second[1], latitudeDirection, v.second[3], longitudeDirection, v.second[8]);
+        return values;
+    }
+	throw std::invalid_argument("Word format is not supported!");	
 }
 
-Route routeFromLog(std::istream &)
+Route routeFromLog(std::istream &v)
 {
-    // Stub definition, needs implementing
+	//Route routeTaken;
+	//std::string sentence
+	// Stub definition, needs implementing
     return {};
+
 }
 
 
